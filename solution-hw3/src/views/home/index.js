1fccad4b2267a1f413c8997371f486fb4ff2dcbc
agschmid <agschmid@andrew.cmu.edu>
2023-09-20 12:18:13 -0400

    update with changes

--- solution-hw3/src/views/home/index.js
+++ solution-hw3/src/views/home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import RollCard from './roll';
+import RollCard from '../cards/roll';
 
-const homepage = () => {
+const Homepage = () => {
     return (
       <div>
           <div className="header-group">
@@ -22,40 +22,40 @@ const homepage = () => {
                 imageAlt = "A frosted cinnamon roll on a plate with cinnamon sticks"
                 rollName="Original cinnamon roll" 
                 rollId="og" 
-                basePrice="2.49" /> 
+                basePrice = {2.49} /> 
           <RollCard 
                 imageURL="assets/products/apple-cinnamon-roll.jpg" 
                 imageAlt = "A cinnamon roll on a plate next to a fork"
                 rollName="Apple cinnamon roll" 
                 rollId="apple" 
-                basePrice="3.49" /> 
+                basePrice= {3.49} /> 
           <RollCard 
                 imageURL="assets/products/raisin-cinnamon-roll.jpg" 
                 imageAlt = "A cinnamon roll with raisins protruding"
                 rollName="Raisin cinnamon roll" 
                 rollId="raisin" 
-                basePrice="2.99" /> 
+                basePrice= {2.99} /> 
           <RollCard 
                 imageURL="assets/products/walnut-cinnamon-roll.jpg" 
                 imageAlt = "A cinnamon roll on a black plate with a 2 prong fork"
                 rollName="Walnut cinnamon roll" 
                 rollId="walnut" 
-                basePrice="3.49" /> 
+                basePrice= {3.49} /> 
           <RollCard 
                 imageURL="assets/products/double-chocolate-cinnamon-roll.jpg" 
                 imageAlt = "A cinnamon roll on a piece of paper with a chocolate glaze and chopped nuts on top"
                 rollName="Double-chocolate cinnamon roll" 
                 rollId="choc" 
-                basePrice="3.99" /> 
+                basePrice= {3.99} /> 
           <RollCard 
                 imageURL="assets/products/strawberry-cinnamon-roll.jpg" 
                 imageAlt = "Multiple cinmamon rolls and straberries skewered by sticks"
                 rollName="Strawberry cinnamon roll" 
                 rollId="strawberry" 
-                basePrice="3.99" /> 
+                basePrice= {3.99} /> 
         </main>
       </div>
     );
 }
 
-export default homepage
+export default Homepage
