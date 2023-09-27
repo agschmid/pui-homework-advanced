78e81cf2e0f465639b33288060fef7ccfdcdc27d
agschmid <agschmid@andrew.cmu.edu>
2023-09-26 22:48:30 -0400

    Update cart area to match hw2

--- solution-hw4/src/views/elements/navbar.js
+++ solution-hw4/src/views/elements/navbar.js
@@ -5,17 +5,39 @@ import './navbar.css';
 class NavBar extends Component {
     render() {
         return (
-          <div className="header-group">
-            <img src="assets/logo/logo-01.svg" alt="A cinnamon bun drawing next to the text 'Bun bun bake shop'" width="400"/>
-            <header>
-              <nav>
-                <a className="bold larger-font hover-hl">PRODUCTS</a>
-                <a className="larger-font hover-hl">CART</a>
-              </nav>
-              <hr/>
-              <span className="largest-font">Our hand-made cinnamon rolls</span>
-            </header>
-          </div>
+              <div className="header-group"> 
+                <img src="./assets/logo/logo-01.svg" alt="A cinnamon bun drawing next to the text 'Bun bun bake shop'" width="400"/>
+                <header>
+                  <nav>
+                    <a className="bold larger-font hover-hl">PRODUCTS</a>
+          
+                    <div className="cart-area">
+
+                      <div className="cart-constant">
+                        <a className="larger-font">CART</a>
+                        <ul className="cart-elements">
+                          <li>0 items</li>
+                          <li>Total: $0.00</li>
+                        </ul>
+                      </div>
+          
+                      <div className="cart-popup">
+                        <span>Added to Cart: <br/><br/></span>
+                        <ul className="popup-list">
+                          <li className="bold"></li>
+                          <li></li>
+                          <li></li>
+                          <li></li> 
+                        </ul>
+                      </div>
+          
+                    </div>
+          
+                  </nav>
+                  <hr/>
+                  <span className="largest-font">Our hand-made cinnamon rolls</span>
+                </header>
+              </div>
         ) 
     }
 }
